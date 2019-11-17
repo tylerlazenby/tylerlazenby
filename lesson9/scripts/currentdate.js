ff89ef07d8cc0b7ed2ce2449f1e511a2fcaea78c
Tyler Lazenby <laz06001@byui.edu>
2019-11-16 22:46:40 -0700

    made week 9 changes

---
+++ lesson9/scripts/currentdate.js
@@ -0,0 +1,31 @@
+"use strict";
+
+let d = new Date();
+let weekday = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+];
+
+let month = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "November",
+    "December"
+];
+
+console.log(d);
+let dateStr = weekday[d.getDay()] + ", " + d.getDate() + " " + month[d.getMonth()] + " " + d.getFullYear();
+document.getElementById("footerDate").innerHTML = dateStr;
+document.getElementById("copyrightYear").innerHTML = d.getFullYear().toString();
